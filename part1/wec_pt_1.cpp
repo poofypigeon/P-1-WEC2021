43e4c8dec019eaa6d85db2cb9c86c4452b1c6e98
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-11-13 10:30:02 -0500

    Probably broke the repo

--- part1/wec_pt_1.cpp
+++ part1/wec_pt_1.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#include <string>
+#include <sstream>
+#include <cctype>
+
 int main () {
 
     return 0;
@@ -15,7 +19,28 @@ int main () {
 
 
 
-double get_time(char letter, bool isUpper) {
-    return 0.0;
+double get_time(std::string line) 
+{
+    uint8_t size = line.length();
+    double time_elapsed{0.0};
+    
+    // don't consider for k=1 or k=0, so just return 0.0
+    
+    if (size > 1) {
+        for (unsigned int k{1}; k < size; k++) {
+            char current_char = line[k];
+            char previous_char= line[k-1];
+
+            time_elapsed += ()
+
+
+        }
+    }
+
+
+    return time_elapsed;
 }
 
+// input of ac
+//
+// zero on the first round, and then some movement distance 
