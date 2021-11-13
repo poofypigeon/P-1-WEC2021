18ae895e7a7662439bfa5787d971b39bffb7ba04
Aiden Fox-Ivey <71291879+Foxnaut@users.noreply.github.com>
2021-11-13 10:57:11 -0500

    Implemented the struct handling in get_time

--- part1/wec_pt_1.cpp
+++ part1/wec_pt_1.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <cctype>
 
-<<<<<<< HEAD
 #include <string>
 #include <sstream>
 #include <cctype>
+#include <math>
 
 inline
 uint8_t char_index(char c) 
@@ -43,16 +43,31 @@ double get_time(std::string line)
     uint8_t size = line.length();
     double time_elapsed{0.0};
     
+    // RULE 1
     // don't consider for k=1 or k=0, so just return 0.0
     
     if (size > 1) {
         for (unsigned int k{1}; k < size; k++) {
             char current_char = line[k];
+            uint8_t current_pos = get_key(current_char).pos;
             char previous_char= line[k-1];
+            uint8_t previous_pos get_key(previous_char).pos;
+            
+            // RULE 2 
+            // determine whether they're on the same key, but not the same letter
+            bool isSameKey = (current_pos == previous_pos); 
 
-            time_elapsed += ()
+            if (isSameKey) {
+                // RULE 3
+                uint8_t num_presses{math::abs((get_key(current_char).num) - get_key(previous_char).num)};
+
+                time_elapsed += num_presses*0.25;
 
 
+            } else if (!isSameKey) {
+                time_elapsed += 0.25;  
+
+            } 
         }
     }
 
